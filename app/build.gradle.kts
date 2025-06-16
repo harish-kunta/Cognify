eacf3ff43cc57eeaaaee3c3d772c9b4271394ec0
Harish Kunta <33089746+harishtanu007@users.noreply.github.com>
2025-06-16 14:05:26 -0700

    Updated home page

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
         applicationId = "com.gigamind.cognify"
         minSdk = 24
         targetSdk = 35
-        versionCode = 2
+        versionCode = 3
         versionName = "0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
