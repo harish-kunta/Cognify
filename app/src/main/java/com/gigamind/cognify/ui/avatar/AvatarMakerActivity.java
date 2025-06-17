e84c6b5d4b83eb8ac971827b59caf260d4a713c0
Harish Kunta <33089746+harish-kunta@users.noreply.github.com>
2025-06-16 18:32:13 -0700

    Add basic avatar maker feature

---
+++ app/src/main/java/com/gigamind/cognify/ui/avatar/AvatarMakerActivity.java
@@ -0,0 +1,26 @@
+package com.gigamind.cognify.ui.avatar;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.gigamind.cognify.R;
+
+public class AvatarMakerActivity extends AppCompatActivity {
+    private ImageView hairView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_avatar_maker);
+
+        hairView = findViewById(R.id.hairView);
+        Button longHairBtn = findViewById(R.id.longHairButton);
+        Button hairBunBtn = findViewById(R.id.hairBunButton);
+
+        longHairBtn.setOnClickListener(v -> hairView.setImageResource(R.drawable.hair_long));
+        hairBunBtn.setOnClickListener(v -> hairView.setImageResource(R.drawable.hair_bun));
+    }
+}
