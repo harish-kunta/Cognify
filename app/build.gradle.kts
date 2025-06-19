0f69cb86c795ba1926b91c0068359a47dee3797a
Harish Kunta <33089746+harishtanu007@users.noreply.github.com>
2025-06-19 06:04:01 -0700

    Release 0.2.6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         applicationId = "com.gigamind.cognify"
         minSdk = 24
         targetSdk = 35
-        versionCode = 4
+        versionCode = 6
         versionName = "0.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
